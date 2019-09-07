85d5838d273964e717e6b7008d95acab8a525253
Sujay Swain <sujay.swain@noreply.github.com>
2019-09-07 09:50:27 -0400

    test

--- Test/tester.py
+++ Test/tester.py
@@ -10,7 +10,7 @@ info = {}
 with open('marquee_config.json', "r") as file:
 	info = json.load(file)
 
-GsSession.use(Environment.PROD, '74d4f25c033e4bd7ad6909ca40f6ad8d', '15c2a007595b37787456949fd6bb35aa23c77b3495ffe449e2c7476f0d7e87f6', scopes=GsSession.Scopes.get_default())
+GsSession.use(Environment.PROD, '74d4f25c0	33e4bd7ad6909ca40f6ad8d', '15c2a007595b37787456949fd6bb35aa23c77b3495ffe449e2c7476f0d7e87f6', scopes=GsSession.Scopes.get_default())
 
 ds = Dataset('USCANFPP_MINI')
 data = ds.get_data(datetime.date(2017, 1, 15), datetime.date(2018, 1, 15), gsid=["75154", "193067", "194688", "902608", "85627"])
