efd1ae9ae9fe43048fb2deaa5c4dbb47af3a0e8f
Sujay Swain <sujay.swain@noreply.github.com>
2019-09-07 21:00:53 -0400

    coll

--- newStuff/News.py
+++ newStuff/News.py
@@ -1,3 +1,32 @@
-import requests
 import json
-print(requests.get("https://newsapi.org/v2/everything?q=\"healthcare\"and\"cybersecurity\"&from=2019-08-07&sortBy=publishedAt&apiKey=dd6d68816cc6455c9d795dbec1f785cc").content)
+import requests
+#import nltk
+#nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
+
+def get_news_1(queries):
+    if (len(queries) < 1):
+        return {}
+    url1 = "https://newsapi.org/v2/everything?q="
+    url2 = "&from=2019-08-08&sortBy=publishedAt&apiKey=dd6d68816cc6455c9d795dbec1f785cc"
+    str = ""
+    for i in range(len(queries)):
+        if (i != 0):
+            str += "and"
+        str += '"' + queries[i] + '"'
+    str = url1 + str + url2
+    return json.loads(requests.get(str).content)
+
+def get_urls_from_news(json):
+    json = json['articles']
+    links = []
+    for item in json:
+        links.append(item['url'])
+    return links
+
+print(get_urls_from_news(get_news_1(['healthcare', 'cybersecurity'])))
+sia = SIA()
+
+def get_sentiment(text):
+    pol_score = sia.polarity_scores(text)
+    return pol_score['compound']
---
+++ newStuff/tester.py
@@ -0,0 +1,71 @@
+# GS Quant documentation available at:
+# https://developer.gs.com/docs/gsquant/guides/getting-started/
+
+import datetime, json, csv, mysql.connector, requests
+
+from gs_quant.data import Dataset
+from gs_quant.session import GsSession, Environment
+
+GsSession.use(Environment.PROD, '74d4f25c03e4bd7ad6909ca40f6ad8d', '15c2a007595b37787456949fd6bb35aa23c77b3495ffe449e2c7476f0d7e87f6', scopes=GsSession.Scopes.get_default())
+
+ds = Dataset('USCANFPP_MINI')
+data = ds.get_data(datetime.date(2017, 1, 15), datetime.date(2018, 1, 15), gsid=["75154", "193067", "194688", "902608", "85627"])
+# print(data.to_csv()) # peek at first few rows of data
+	
+data = data.values.tolist()
+
+sql_config = {}
+
+with open('sql_config.json', "r") as file:
+	sql_config = json.load(file)
+
+conn = mysql.connector.connect(**sql_config)
+cursor = conn.cursor()
+
+def addrow(r):
+	str = "insert into csvdb (a, b, c, d, e, f)\n \
+	values(%s, %s, %s, %s, %s, %s)"
+	try:
+		cursor.execute(str, tuple(r))
+	except mysql.connector.Error as error:
+		print("db error:", error)
+
+for row in data:
+	addrow(row)
+	
+conn.commit()
+conn.close()
+cursor.close()
+
+'''
+table csvdb (
+	a varchar(50),
+	b varchar(50),
+	c varchar(50),
+	d varchar(50),
+	e varchar(50),
+	f varchar(50)
+)
+'''
+
+auth_data = {
+    'grant_type'    : 'client_credentials',
+    'client_id'     : '74d4f25c033e4bd7ad6909ca40f6ad8d',
+    'client_secret' : '15c2a007595b37787456949fd6bb35aa23c77b3495ffe449e2c7476f0d7e87f6',
+    'scope'         : 'read_product_data'
+};
+# create session instance
+session = requests.Session()
+gsid = "10516"
+auth_request = session.post("https://idfs.gs.com/as/token.oauth2", data = auth_data)
+access_token_dict = json.loads(auth_request.text)
+access_token = access_token_dict["access_token"]
+
+# update session headers with access token
+session.headers.update({"Authorization":"Bearer "+ access_token})
+
+request_url = "https://api.marquee.gs.com/v1/assets/" + gsid
+
+request = session.get(url=request_url)
+results = json.loads(request.text)
+print(results)
